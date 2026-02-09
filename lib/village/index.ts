486e4084f7d30f50630b7f867ff544b9f3f718f2
Integration Lead <shalomolahs@moikaslabs.com>
2026-02-09 06:14:48 +0000

    🏘️ Integrate generative village: buildings, agents, schedules, social, memory, admin

---
+++ lib/village/index.ts
@@ -0,0 +1,4 @@
+export { BUILDINGS, getBuildingById, getBuildingByRole, getRandomEntrancePosition, calculateDistance, getNearestBuilding } from './buildings';
+export { ScheduleGenerator, scheduleGenerator, HOURS_TO_MS } from './schedules';
+export { SocialSystem, socialSystem } from './social';
+export { Pathfinder, pathfinder, lerp, lerpPosition, distance, distanceXZ } from './pathfinding';
